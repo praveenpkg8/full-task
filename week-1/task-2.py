59fac6ff149634272320117468058772901626b7
Praveen Kumar <30780099+praveenpkg8@users.noreply.github.com>
2019-06-06 22:11:24 +0530

    dek

--- week-1/task-2.py
+++
@@ -1,7 +0,0 @@
-string_list = "hello wor36@$^&#$#^$$^ld"
-sorted_set_character = sorted(set(string_list))
-key_pair = lambda y, x: {y: x}
-output_dict = {}
-for s in sorted_set_character:
-    output_dict.update(key_pair(s, string_list.count(s)))
-print(output_dict)
